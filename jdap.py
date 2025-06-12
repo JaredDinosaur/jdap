37d3287bb2ed73a21bbbc259e71f23a15cbd8227
Jared Dinosaur <61011316+JaredDinosaur@users.noreply.github.com>
2025-06-12 15:02:33 +0100

    Add files via upload

---
+++ jdap.py
@@ -0,0 +1,20 @@
+import os
+print("This script will not work without an internet connection!")
+print("Please run this script as a user with sudo privileges (NOT root)!!!")
+check=input("Enter 'ready' to run the script, or anything else to quit: ")
+if check=="ready":
+    os.system("sudo nano /etc/pacman.conf")
+    print("1) None")
+    print("2) GRUB")
+    print("3) Limine")
+    bledit=input("Enter a config to edit: ")
+    if bledit=="2":
+        os.system("sudo nano /boot/grub/grub.conf")
+    if bledit=="3":
+        os.system("sudo nano /boot/limine.conf")
+    os.system("git clone https://aur.archlinux.org/yay.git")
+    os.system("cd yay")
+    os.system("makepkg -si")
+    os.system("yay -S firefox flatpak screenfetch tree htop partitionmanager plymouth vlc packagekit base-devel ark git")
+    os.system("clear")
+    print("Script complete!")
