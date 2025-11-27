f911a95c352151955760815458391621a8578138
Jared Dinosaur <61011316+JaredDinosaur@users.noreply.github.com>
2025-11-27 18:28:36 +0000

    Add files via upload

--- jdap.sh
+++ jdap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo This script will not work without an internet connection!
-echo Please run this script as a user with sudo privileges (NOT root)!
-echo Press Enter to continue or Ctrl+C to exit...
+echo "This script will not work without an internet connection!"
+echo "Please run this script as a user with sudo privileges (NOT root)!"
+echo "Press Enter to continue or Ctrl+C to exit..."
 read
 
 sudo nano /etc/pacman.conf
-echo 1) Skip
-echo 2) Edit GRUB configuration
-echo 3) Edit Limine configuration
-echo Enter your choice:
+echo "1) Skip"
+echo "2) Edit GRUB configuration"
+echo "3) Edit Limine configuration"
+echo "Enter your choice:"
 read choice
 case $choice in
     2)
@@ -30,5 +30,5 @@ makepkg -si
 yay -S firefox flatpak neofetch screenfetch fastfetch tree htop btop partitionmanager plymouth vlc packagekit base-devel ark waybar hyprpaper thunar wofi
 clear
 echo
-echo Done!
+echo "Done!"
 echo
